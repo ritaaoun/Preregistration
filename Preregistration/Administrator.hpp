11790361f3b13c8a9f3e147d20681ace0fc0bb96
Rita Aoun <rita-aoun@hotmail.com>
2017-04-11 23:54:17 +0300

    AbstractUser

---
+++ Preregistration/Administrator.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "AbstractUser.hpp"
+
+class Administrator : public AbstractUser {
+
+};
---
+++ Preregistration/Enum.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace Term {
+	enum Term {Fall, Winter, Spring, Summer, TERM_END};
+};
