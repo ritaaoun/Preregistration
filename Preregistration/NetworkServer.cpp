e2b18324cd9938ef12353bfd67e7d8fdf795c84d
caj07 <caj07@mail.aub.edu>
2017-05-04 21:34:48 +0300

    removed useless classes fixed link error cdecl and fixed server

---
+++ Preregistration/NetworkServer.cpp
@@ -0,0 +1,132 @@
+#include "NetworkServer.hpp"
+#include "Server.hpp"
+
+NetworkServer::NetworkServer()
+{
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed with error: %d\n", iResult);
+		return;
+	}
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+
+	// Resolve the server address and port
+	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed with error: %d\n", iResult);
+		WSACleanup();
+		return;
+	}
+
+	// Create a SOCKET for connecting to server
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (ListenSocket == INVALID_SOCKET) {
+		printf("socket failed with error: %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return;
+	}
+
+	// Setup the TCP listening socket
+	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+		printf("bind failed with error: %d\n", WSAGetLastError());
+		freeaddrinfo(result);
+		closesocket(ListenSocket);
+		WSACleanup();
+		return;
+	}
+
+	freeaddrinfo(result);
+
+	iResult = listen(ListenSocket, SOMAXCONN);
+	if (iResult == SOCKET_ERROR) {
+		printf("listen failed with error: %d\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return;
+	}
+}
+
+NetworkServer::~NetworkServer()
+{
+}
+
+NetworkServer::NetworkServer(const NetworkServer & other)
+{
+}
+
+NetworkServer & NetworkServer::operator=(const NetworkServer & rhs)
+{
+	// TODO: insert return statement here
+	return *this;
+}
+
+int NetworkServer::acceptClient()
+{
+	ClientSocket = accept(ListenSocket, NULL, NULL);
+	if (ClientSocket == INVALID_SOCKET) {
+		printf("accept failed with error: %d\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+	
+	// Receive until the peer shuts down the connection
+	do {
+
+		iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+
+		if (iResult > 0) {
+			
+			std::string result ="";
+			result = Server::getInstance().serverInterface.callFunction(std::string(recvbuf));
+
+			// Echo the buffer back to the sender
+			iSendResult = send(ClientSocket, result.c_str(), result.size(), 0);
+			if (iSendResult == SOCKET_ERROR) {
+				printf("send failed with error: %d\n", WSAGetLastError());
+				closesocket(ClientSocket);
+				WSACleanup();
+				return 1;
+			}
+		}
+		else if (iResult == 0)
+			printf("Connection closing...\n");
+		else {
+			printf("recv failed with error: %d\n", WSAGetLastError());
+			closesocket(ClientSocket);
+			WSACleanup();
+			return 1;
+		}
+
+	} while (iResult > 0);
+
+	// shutdown the connection since we're done
+	iResult = shutdown(ClientSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		printf("shutdown failed with error: %d\n", WSAGetLastError());
+		closesocket(ClientSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	// cleanup
+	closesocket(ClientSocket);
+	WSACleanup();
+	return 0;
+}
+
+void NetworkServer::closeListener()
+{
+
+	// No longer need server socket
+	closesocket(ListenSocket);
+}
+
--- Preregistration/Server.cpp
+++ Preregistration/Server.cpp
@@ -1,8 +1,8 @@
 #include "Server.hpp"
-#include "myHostInfo.h"
+#include "NetworkServer.hpp"
 #include "SqliteRepository.hpp"
 
-Server::Server() : MAX_THREADS(10), repository(&SqliteRepository::getInstance()), data(), serverInterface()
+Server::Server() : repository(&SqliteRepository::getInstance()), data(), serverInterface(),netServer()
 {
 	startlistener();	
 }
@@ -18,13 +18,9 @@ Server & Server::operator=(const Server & rhs)
 	return *this;
 }
 
-bool Server::closelistener()
+void Server::closelistener()
 {
-	for (int i = 0;i < MAX_THREADS;i++)
-	{
-		threads[i].join();
-	}
-	return true;
+	netServer.closeListener();
 }
 
 bool Server::start()
@@ -32,68 +28,12 @@ bool Server::start()
 	return false;
 }
 
-bool Server::startlistener()
+void Server::startlistener()
 {
-	winLog << endl;
-	winLog << "Retrieve the local host name and address:" << endl;
-
-	myHostInfo uHostAddress;
-	string localHostName = uHostAddress.getHostName();
-	string localHostAddr = uHostAddress.getHostIPAddress();
-
-	winLog << "		==> Name: " << localHostName << endl;
-	winLog << "		==> Address: " << localHostAddr << endl;
-
-	// open socket on the local host
-	myTcpServer = myTcpSocket(PORTNUM);
-	winLog << "server configuation: " << endl;
-	winLog << myTcpServer;
-
-	myTcpServer.bindSocket();
-	winLog << endl << "server finishes binding process... " << endl;
-
-	myTcpServer.listenToClient();
-	winLog << "server is listening to the port ... " << endl;
-
-	// wait to accept a client connection.  
-	// processing is suspended until the client connects
-	winLog << "server is waiting for client connnection ... " << endl;
-
-	
-	for (int i = 0;i < MAX_THREADS;i++)
+	while (2.0)
 	{
-		//threads[i] = std::thread(&listen,this);
-	}
-
-	return true;
-}
-
-
-void Server::listen()
-{
-	while (2.0) 
-	{
-		myTcpSocket* client;    // connection dedicated for client communication
-		string clientHost;      // client name etc. 
-
-		client = myTcpServer.acceptClient(clientHost);
-
-		winLog << endl << "==> A client from [" << clientHost << "] is connected!" << endl << endl;
-
-		while (1)
-		{
-			string clientMessageIn = "";
-			string result = "";
-
-			// receive from the client
-
-			int numBytes = client->receiveMessage(clientMessageIn);
-			if (numBytes == -99) break;
-
-			result = serverInterface.callFunction(clientMessageIn);
-
-			// send to the client		
-			client->sendMessage(result);
-		}
+		netServer.acceptClient();
 	}
 }
+
+
