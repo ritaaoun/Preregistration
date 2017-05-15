c2aee33dbbbff1f73240f2a2f6087cb573a0a515
caj07 <caj07@mail.aub.edu>
2017-05-15 20:13:07 +0300

    fixed db bug

--- Preregistration/SqliteRepository.cpp
+++ Preregistration/SqliteRepository.cpp
@@ -626,7 +626,7 @@ int SqliteRepository::createCourse(const Course * course) const
 	std::string sql = "INSERT INTO COURSE (DEPARTMENTID, CODE, NAME, DESCRIPTION, CREDITS, STATUS) VALUES ( '" +
 		std::to_string(course->getDepartmentId()) + "', '" + course->getCode() + "', '" + course->getName() + "', '" +
 		course->getDescription() + "', '" + std::to_string(course->getNumberOfCredits()) + "', '" +
-		std::to_string(course->getStatus()) + "'";
+		std::to_string(course->getStatus()) + "')";
 	if (execute(sql))
 	{
 		std::string sql = "SELECT MAX(ID) FROM COURSE";
